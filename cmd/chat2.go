8dab8e5444eb47bf167b34b65a798d1350195e13
huoju <virushuo@gmail.com>
2021-01-11 19:12:53 -0500

    chat with kad-dht and routing discovery

---
+++ cmd/chat2.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+    "time"
+	"os"
+	"sync"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	"github.com/libp2p/go-libp2p-discovery"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/routing"
+	"github.com/libp2p/go-libp2p-kad-dht/dual"
+
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	multiaddr "github.com/multiformats/go-multiaddr"
+	//logging "github.com/whyrusleeping/go-logging"
+
+	"github.com/ipfs/go-log"
+)
+
+var logger = log.Logger("rendezvous")
+
+func handleStream(stream network.Stream) {
+	logger.Info("Got a new stream!")
+
+	// Create a buffer stream for non blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+	go readData(rw)
+	go writeData(rw)
+
+	// 'stream' will stay open until you close it (or the other side closes it).
+}
+
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, err := rw.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from buffer")
+			panic(err)
+		}
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from stdin")
+			panic(err)
+		}
+
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		if err != nil {
+			fmt.Println("Error writing to buffer")
+			panic(err)
+		}
+		err = rw.Flush()
+		if err != nil {
+			fmt.Println("Error flushing buffer")
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	//log.SetAllLoggers(logging.WARNING)
+	log.SetLogLevel("rendezvous", "info")
+	help := flag.Bool("h", false, "Display Help")
+	config, err := ParseFlags()
+	if err != nil {
+		panic(err)
+	}
+
+	if *help {
+		fmt.Println("This program demonstrates a simple p2p chat application using libp2p")
+		fmt.Println()
+		fmt.Println("Usage: Run './chat in two different terminals. Let them connect to the bootstrap nodes, announce themselves and connect to the peers")
+		flag.PrintDefaults()
+		return
+	}
+
+	ctx := context.Background()
+
+	var ddht *dual.DHT
+	var routingDiscovery *discovery.RoutingDiscovery
+
+	//routing := libp2p.Routing(func(host host.Host) (routing.PeerRouting, error) {
+	//	return dual.New(ctx, host)
+	//})
+
+	routing := libp2p.Routing(func(host host.Host) (routing.PeerRouting, error) {
+		var err error
+		ddht, err = dual.New(ctx, host)
+		routingDiscovery = discovery.NewRoutingDiscovery(ddht)
+
+		return ddht, err
+	})
+
+	// libp2p.New constructs a new libp2p Host. Other options can be added
+	// here.
+	host, err := libp2p.New(ctx,
+		routing,
+		libp2p.ListenAddrs([]multiaddr.Multiaddr(config.ListenAddresses)...),
+	)
+	if err != nil {
+		panic(err)
+	}
+	logger.Info("Host created. We are:", host.ID())
+	logger.Info(host.Addrs())
+
+	// Set a function as stream handler. This function is called when a peer
+	// initiates a connection and starts a stream with this peer.
+	host.SetStreamHandler(protocol.ID(config.ProtocolID), handleStream)
+
+	// Start a DHT, for use in peer discovery. We can't just make a new DHT
+	// client because we want each peer to maintain its own local copy of the
+	// DHT, so that the bootstrapping node of the DHT can go down without
+	// inhibiting future peer discovery.
+	kademliaDHT, err := dht.New(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+
+    fmt.Printf("[*] Your Bootstrap ID Is: /ip4/127.0.0.1/tcp/4212/p2p/%s\n"  , host.ID().Pretty())
+	// Bootstrap the DHT. In the default configuration, this spawns a Background
+	// thread that will refresh the peer table every five minutes.
+	logger.Debug("Bootstrapping the DHT")
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+
+	// Let's connect to the bootstrap nodes first. They will tell us about the
+	// other nodes in the network.
+	var wg sync.WaitGroup
+	for _, peerAddr := range config.BootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerinfo); err != nil {
+				logger.Warning(err)
+			} else {
+				logger.Info("Connection established with bootstrap node:", *peerinfo)
+			}
+		}()
+	}
+	wg.Wait()
+
+
+	// We use a rendezvous point "meet me here" to announce our location.
+	// This is like telling your friends to meet you at the Eiffel Tower.
+	logger.Info("Announcing ourselves...")
+	discovery.Advertise(ctx, routingDiscovery, config.RendezvousString)
+	//routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
+	//discovery.Advertise(ctx, routingDiscovery, config.RendezvousString)
+	logger.Info("Successfully announced!")
+
+	// Now, look for others who have announced
+	// This is like your friend telling you the location to meet you.
+	logger.Info("Searching for other peers...")
+    fmt.Println(config.RendezvousString)
+
+	pctx, _ := context.WithTimeout(ctx, time.Second*10)
+	peers, err := discovery.FindPeers(pctx, routingDiscovery, config.RendezvousString)
+	if err != nil {
+		panic(err)
+	}
+
+
+	for _, peer := range peers {
+		if peer.ID == host.ID() {
+			continue
+		}
+		logger.Debug("Found peer:", peer)
+
+		logger.Debug("Connecting to:", peer)
+		stream, err := host.NewStream(ctx, peer.ID, protocol.ID(config.ProtocolID))
+
+		if err != nil {
+			logger.Warning("Connection failed:", err)
+			continue
+		} else {
+			rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+			go writeData(rw)
+			go readData(rw)
+		}
+
+		logger.Info("Connected to:", peer)
+	}
+
+	select {}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module huo-ju/quorum
 go 1.15
 
 require (
-	github.com/libp2p/go-libp2p v0.13.0 // indirect
+	github.com/libp2p/go-libp2p v0.13.0
+	github.com/libp2p/go-libp2p-core v0.8.0
+	github.com/libp2p/go-libp2p-kad-dht v0.11.1 // indirect
 	github.com/spf13/viper v1.7.1 // indirect
 )
