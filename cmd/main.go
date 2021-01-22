b739a6688a7d9c20102693a7cb32263f9d8ead0d
huoju <virushuo@gmail.com>
2021-01-22 16:23:56 -0500

    code clean

--- cmd/main.go
+++ cmd/main.go
@@ -28,16 +28,6 @@ import (
     "github.com/huo-ju/quorum/internal/pkg/flags"
 )
 
-//type addrList []maddr.Multiaddr
-//type Config struct {
-//	RendezvousString string
-//	BootstrapPeers   addrList
-//	ListenAddresses  string
-//	ProtocolID       string
-//    IsBootstrap     bool
-//    PeerName        string
-//}
-
 type Keys struct{
 	PrivKey p2pcrypto.PrivKey
 	PubKey p2pcrypto.PubKey
@@ -46,37 +36,6 @@ type Keys struct{
 var sub *pubsub.Subscription
 var ps *pubsub.PubSub
 var ShareTopic string
-
-//func ParseFlags() (Config,error) {
-//    config := Config{ProtocolID:"/quorum/1.0.0"}
-//	flag.StringVar(&config.RendezvousString, "rendezvous", "some unique string",
-//		"Unique string to identify group of nodes. Share this with your friends to let them connect with you")
-//	flag.Var(&config.BootstrapPeers, "peer", "Adds a peer multiaddress to the bootstrap list")
-//	flag.StringVar(&config.ListenAddresses, "listen", "/ip4/127.0.0.1/tcp/4215", "Adds a multiaddress to the listen list")
-//	flag.StringVar(&config.PeerName, "peername", "peer", "peername")
-//    flag.BoolVar(&config.IsBootstrap, "bootstrap", false, "run a bootstrap node")
-//	flag.Parse()
-//
-//	return config, nil
-//}
-
-//func (al *addrList) String() string {
-//	strs := make([]string, len(*al))
-//	for i, addr := range *al {
-//		strs[i] = addr.String()
-//	}
-//	return strings.Join(strs, ",")
-//}
-//
-//func (al *addrList) Set(value string) error {
-//	addr, err := maddr.NewMultiaddr(value)
-//	if err != nil {
-//		return err
-//	}
-//	*al = append(*al, addr)
-//	return nil
-//}
-
 
 func StringsToAddrs(addrStrings []string) (maddrs []maddr.Multiaddr, err error) {
 	for _, addrString := range addrStrings {
