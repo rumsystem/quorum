f68f715f7ef55183bab53c993c2074fd8063ce33
cmcater <cmcater@gmail.com>
2021-04-22 14:22:29 -0400

    add getblk

--- internal/pkg/api/getblk.go
+++ internal/pkg/api/getblk.go
@@ -1,12 +1,36 @@
 package api
 
 import (
+	"github.com/go-playground/validator/v10"
+	chain "github.com/huo-ju/quorum/internal/pkg/chain"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
+type GetBlockParam struct {
+	BlockId string `from:"block_id" json:"block_id" validate:"required"`
+}
+
 func (h *Handler) GetBlock(c echo.Context) (err error) {
 	output := make(map[string]string)
-	output[ERROR_INFO] = "Not implement yet"
-	return c.JSON(http.StatusOK, output)
+
+	validate := validator.New()
+	params := new(GetBlockParam)
+	if err = c.Bind(params); err != nil {
+		output[ERROR_INFO] = err.Error()
+		return c.JSON(http.StatusBadRequest, output)
+	}
+
+	if err = validate.Struct(params); err != nil {
+		output[ERROR_INFO] = err.Error()
+		return c.JSON(http.StatusBadRequest, output)
+	}
+
+	block, err := chain.GetDbMgr().GetRawBlock(params.BlockId)
+	if err != nil {
+		output[ERROR_INFO] = err.Error()
+		return c.JSON(http.StatusBadRequest, output)
+	}
+
+	return c.JSON(http.StatusOK, block)
 }
