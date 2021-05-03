d3f3f76fd9c38a2878a57b97366b02e2a7281933
huoju <virushuo@gmail.com>
2021-05-03 16:28:59 -0400

    clean unused data package

--- internal/pkg/data/actor.go
+++
@@ -1,6 +0,0 @@
-package data
-
-type Actor struct {
-	Type string `json:"type"`
-	Name string `json:"name"`
-}
--- internal/pkg/data/object.go
+++
@@ -1,9 +0,0 @@
-package data
-
-import ()
-
-type Object struct {
-	Type    string `json:"type"`
-	Name    string `json:"name"`
-	Content string `json:"content"`
-}
