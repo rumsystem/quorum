7c5c0e0237da2630b29ea9e4e634b91b77ca5897
huoju <virushuo@gmail.com>
2021-05-26 22:49:24 -0400

    testcases updated

--- testnode/setup.go
+++ testnode/setup.go
@@ -9,7 +9,6 @@ import (
 	"github.com/huo-ju/quorum/internal/pkg/utils"
 	connmgr "github.com/libp2p/go-libp2p-connmgr"
 	peer "github.com/libp2p/go-libp2p-core/peer"
-	discovery "github.com/libp2p/go-libp2p-discovery"
 	"io/ioutil"
 	"log"
 	"os"
@@ -21,7 +20,7 @@ func Run2nodes(ctx context.Context, mockRendezvousString string) (*p2p.Node, *p2
 	mockbootstrapaddr := "/ip4/127.0.0.1/tcp/8520"
 	mockbootstrapnodekeys, err := localcrypto.NewKeys()
 	listenaddresses, _ := utils.StringsToAddrs([]string{mockbootstrapaddr})
-	node, err := p2p.NewNode(ctx, mockbootstrapnodekeys.PrivKey, connmgr.NewConnManager(1000, 50000, 30), listenaddresses, "")
+	node, err := p2p.NewNode(ctx, true, mockbootstrapnodekeys.PrivKey, connmgr.NewConnManager(1000, 50000, 30), listenaddresses, "")
 	if err != nil {
 		return nil, nil, nil, nil, nil, nil, err
 	}
@@ -33,26 +32,26 @@ func Run2nodes(ctx context.Context, mockRendezvousString string) (*p2p.Node, *p2
 
 	mockpeer1nodekeys, err := localcrypto.NewKeys()
 	peer1listenaddresses, _ := utils.StringsToAddrs([]string{"/ip4/127.0.0.1/tcp/8551"})
-	node1, err := p2p.NewNode(ctx, mockpeer1nodekeys.PrivKey, connmgr.NewConnManager(10, 200, 60), peer1listenaddresses, "")
+	node1, err := p2p.NewNode(ctx, false, mockpeer1nodekeys.PrivKey, connmgr.NewConnManager(10, 200, 60), peer1listenaddresses, "")
 	if err != nil {
 		return nil, nil, nil, nil, nil, nil, err
 	}
 	_ = node1.Bootstrap(ctx, *defaultnodeconfig)
-	log.Println("Announcing peer1...")
-	discovery.Advertise(ctx, node1.RoutingDiscovery, defaultnodeconfig.RendezvousString)
-	log.Println("Successfully announced peer1!")
+	//log.Println("Announcing peer1...")
+	//discovery.Advertise(ctx, node1.RoutingDiscovery, defaultnodeconfig.RendezvousString)
+	//log.Println("Successfully announced peer1!")
 
 	//TODO: use peerID to instead the RendezvousString, anyone can claim to this RendezvousString now"
 	mockpeer2nodekeys, err := localcrypto.NewKeys()
 	peer2listenaddresses, _ := utils.StringsToAddrs([]string{"/ip4/127.0.0.1/tcp/8552"})
-	node2, err := p2p.NewNode(ctx, mockpeer2nodekeys.PrivKey, connmgr.NewConnManager(10, 200, 60), peer2listenaddresses, "")
+	node2, err := p2p.NewNode(ctx, false, mockpeer2nodekeys.PrivKey, connmgr.NewConnManager(10, 200, 60), peer2listenaddresses, "")
 	if err != nil {
 		return nil, nil, nil, nil, nil, nil, err
 	}
 	node2.Bootstrap(ctx, *defaultnodeconfig)
-	log.Println("Announcing peer2...")
-	discovery.Advertise(ctx, node2.RoutingDiscovery, defaultnodeconfig.RendezvousString)
-	log.Println("Successfully announced peer2")
+	//log.Println("Announcing peer2...")
+	//discovery.Advertise(ctx, node2.RoutingDiscovery, defaultnodeconfig.RendezvousString)
+	//log.Println("Successfully announced peer2")
 	return node, node1, node2, mockbootstrapnodekeys, mockpeer1nodekeys, mockpeer2nodekeys, nil
 }
 
