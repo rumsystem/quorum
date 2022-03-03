4b49bcfa72fdd7acc7308a9487cb7027fc346a55
Yongsheng Xu <chuxdesign@hotmail.com>
2022-03-03 20:52:53 +0800

    feat(wasm): add GetChainTrxDenyList APi

--- pkg/wasm/api/getchaintrxallowlist.go
+++ pkg/wasm/api/getchaintrxallowlist.go
@@ -1,3 +1,6 @@
+//go:build js && wasm
+// +build js,wasm
+
 package api
 
 import "github.com/rumsystem/quorum/internal/pkg/handlers"
--- pkg/wasm/register.go
+++ pkg/wasm/register.go
@@ -301,6 +301,21 @@ func RegisterJSFunctions() {
 		return Promisefy(handler)
 	}))
 
+	js.Global().Set("GetChainTrxDenyList", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		groupId := args[0].String()
+		handler := func() (map[string]interface{}, error) {
+			ret := make(map[string]interface{})
+			res, err := quorumAPI.GetChainTrxDenyList(groupId)
+			if err != nil {
+				return ret, err
+			}
+			retBytes, err := json.Marshal(res)
+			json.Unmarshal(retBytes, &ret)
+			return ret, nil
+		}
+		return Promisefy(handler)
+	}))
+
 	js.Global().Set("GetAppConfigKeyList", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		groupId := args[0].String()
 		handler := func() (map[string]interface{}, error) {
