7bd64a1fc2f47d2b2c5cc01ea6735c8dbd637da6
Yongsheng Xu <chuxdesign@hotmail.com>
2022-03-08 17:32:54 +0800

    feat(cli): add list render function

--- cmd/cli/ui/page_chain_config.go
+++ cmd/cli/ui/page_chain_config.go
@@ -1,10 +1,16 @@
 package ui
 
 import (
+	"fmt"
 	"runtime"
+	"strconv"
+	"time"
 
 	"code.rocketnine.space/tslocum/cbind"
 	"code.rocketnine.space/tslocum/cview"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rumsystem/quorum/cmd/cli/api"
+	"github.com/rumsystem/quorum/internal/pkg/handlers"
 )
 
 var chainConfigPage = cview.NewFlex()
@@ -39,9 +45,9 @@ func initChainConfigPageHandlers() {
 	denyListHandler := cbind.NewConfiguration()
 	if runtime.GOOS == "windows" {
 		// windows will set extra shift mod somehow
-		denyListHandler.Set("Shift+L", wrapQuorumKeyFn(focusAllowListView))
+		denyListHandler.Set("Shift+H", wrapQuorumKeyFn(focusAllowListView))
 	} else {
-		denyListHandler.Set("L", wrapQuorumKeyFn(focusAllowListView))
+		denyListHandler.Set("H", wrapQuorumKeyFn(focusAllowListView))
 	}
 	denyListHandler.Set("Esc", wrapQuorumKeyFn(focusChainConfigRootView))
 	chainDenyListView.SetInputCapture(denyListHandler.Capture)
@@ -83,5 +89,97 @@ func ChainConfigPage(groupId string) {
 }
 
 func ChainConfigRefreshAll(groupId string) {
+	go goGetChainAllowList(groupId)
+	go goGetChainDenyList(groupId)
+	go goGetChainAuthMode(groupId)
+}
+
+func goGetChainAllowList(groupId string) {
+	data, err := api.GetChainAllowList(groupId)
+	checkFatalError(err)
+
+	renderListView(data, chainAllowListView)
+}
+
+func goGetChainDenyList(groupId string) {
+	data, err := api.GetChainDenyList(groupId)
+	checkFatalError(err)
+
+	renderListView(data, chainDenyListView)
+}
+
+func renderListView(data []*handlers.ChainSendTrxRuleListItem, view *cview.TextView) {
+	view.Clear()
+
+	for i, each := range data {
+		fmt.Fprintf(view, "[\"%d\"]%s\n", i, time.Unix(0, each.TimeStamp))
+		fmt.Fprintf(view, "Pubkey: %s\n", each.Pubkey)
+		fmt.Fprintf(view, "GroupOwnerPubkey: %s\n", each.GroupOwnerPubkey)
+		fmt.Fprintf(view, "GroupOwnerSign: %s\n", each.GroupOwnerSign)
+		fmt.Fprintf(view, "Trx Type: %s\n", each.TrxType)
+		fmt.Fprintf(view, "Memo: %s\n", each.Memo)
+		fmt.Fprintf(view, "\n\n")
+	}
+
+	focusLast := func() {
+		minNum := 0
+		maxNum := len(data) - 1
+
+		curSelection := view.GetHighlights()
+		tag := minNum
+		if len(curSelection) > 0 {
+			tag, _ = strconv.Atoi(curSelection[0])
+			tag += 1
+		}
+		if tag >= minNum && tag <= maxNum {
+			view.Highlight(strconv.Itoa(tag))
+			view.ScrollToHighlight()
+		}
+	}
+	focusNext := func() {
+		minNum := 0
+		maxNum := len(data) - 1
+
+		curSelection := view.GetHighlights()
+		tag := minNum
+		if len(curSelection) > 0 {
+			tag, _ = strconv.Atoi(curSelection[0])
+			tag -= 1
+		}
+		if tag >= minNum && tag <= maxNum {
+			view.Highlight(strconv.Itoa(tag))
+			view.ScrollToHighlight()
+		}
+	}
+
+	view.SetDoneFunc(func(key tcell.Key) {
+		switch key {
+		case tcell.KeyEsc:
+			view.Highlight("")
+			cmdInput.SetLabel("")
+			cmdInput.SetText("")
+		case tcell.KeyEnter:
+			curSelection := view.GetHighlights()
+			if len(curSelection) > 0 {
+
+			}
+
+		case tcell.KeyTab:
+			focusNext()
+		case tcell.KeyBacktab:
+			focusLast()
+		default:
+		}
+	})
+}
+
+func goGetChainAuthMode(groupId string) {
 	// TODO:
+	// POST
+	// ANNOUNCE
+	// REQ_BLOCK_FORWARD
+	// REQ_BLOCK_BACKWARD
+	// ASK_PEERID
+	// BLOCK_SYNCED
+	// BLOCK_PRODUCED
 }
