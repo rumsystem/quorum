b2a29a36b4ab76e2fe3546399af754212c329164
cmcater <cmcater@gmail.com>
2022-03-08 13:06:07 -0500

    remote unused structure, remote go routine when stopSyncAllGroup in GroupMgr

--- internal/pkg/chain/syncer.go
+++ internal/pkg/chain/syncer.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	iface "github.com/rumsystem/quorum/internal/pkg/chaindataciface"
@@ -61,23 +60,6 @@ func (syncer *Syncer) Init(group *Group, cdnIface iface.ChainDataHandlerIface) {
 
 func (syncer *Syncer) SetRumExchangeTestMode() {
 	syncer.rumExchangeTestMode = true
-}
-
-type TAtomBool struct{ flag int32 }
-
-func (b *TAtomBool) Set(value bool) {
-	var i int32 = 0
-	if value {
-		i = 1
-	}
-	atomic.StoreInt32(&(b.flag), int32(i))
-}
-
-func (b *TAtomBool) Get() bool {
-	if atomic.LoadInt32(&(b.flag)) != 0 {
-		return true
-	}
-	return false
 }
 
 func (syncer *Syncer) SyncLocalBlock(blockId, nodename string) error {
