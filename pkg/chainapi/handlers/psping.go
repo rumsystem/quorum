b05157902113a2173ab4499ddf3bb70a76fa45ab
Yongsheng Xu <chuxdesign@hotmail.com>
2022-05-11 13:03:52 +0800

    feat: add Ping method (modified IPFS version)

--- pkg/chainapi/handlers/psping.go
+++ pkg/chainapi/handlers/psping.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/rumsystem/quorum/internal/pkg/conn/p2p"
@@ -12,6 +14,7 @@ type PingResp struct {
 	TTL [10]int64 `json:"ttl"`
 }
 
+// pubsub ping
 func Ping(ps *pubsub.PubSub, id peer.ID, remote string) (*PingResp, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -23,4 +26,36 @@ func Ping(ps *pubsub.PubSub, id peer.ID, remote string) (*PingResp, error) {
 	}
 
 	return &PingResp{res}, nil
+}
+
+type P2PPingResp struct {
+	TTL [10]int64 `json:"ttl"`
+}
+
+// p2p ping
+func P2PPing(h host.Host, remote string) (*P2PPingResp, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ps := p2p.NewPingService(h)
+
+	resp := &P2PPingResp{}
+
+	for _, pid := range h.Peerstore().Peers() {
+		if pid.String() == remote {
+			ch := ps.Ping(ctx, pid)
+
+			for i := 0; i < 10; i++ {
+				res := <-ch
+				if res.Error != nil {
+					resp.TTL[i] = 0
+				} else {
+					resp.TTL[i] = res.RTT.Milliseconds()
+				}
+			}
+			return resp, nil
+		}
+	}
+
+	return nil, fmt.Errorf("peer not found")
 }
