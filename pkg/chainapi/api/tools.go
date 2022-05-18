ea21e94f28036cf456d4fcede9a7340e45f7cc28
huoju <virushuo@gmail.com>
2022-05-18 19:56:18 -0400

    add tools api
    api /api/v1/tools/pubkeytoaddr

---
+++ pkg/chainapi/api/tools.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	localcrypto "github.com/rumsystem/keystore/pkg/crypto"
+	"net/http"
+)
+
+type PubkeyParam struct {
+	EncodedPubkey string `from:"encoded_pubkey" json:"encoded_pubkey" validate:"required"`
+}
+
+// @Tags Tools
+// @Summary PubkeyToEthaddr
+// @Description Convert a based64 encoded libp2p pubkey to the eth address
+// @Accept json
+// @Produce json
+// @Param data body PubkeyParam true "PubkeyParam"
+// @Success 200 {object} map[string]string
+// @Router /api/v1/tools/pubkeytoaddr [post]
+func (h *Handler) PubkeyToEthaddr(c echo.Context) (err error) {
+	var input PubkeyParam
+	output := make(map[string]string)
+
+	if err = c.Bind(&input); err != nil {
+		output[ERROR_INFO] = err.Error()
+		return c.JSON(http.StatusBadRequest, output)
+	}
+
+	fmt.Println()
+	ethaddr, err := localcrypto.Libp2pPubkeyToEthaddr(input.EncodedPubkey)
+	if err != nil {
+		output[ERROR_INFO] = err.Error()
+		return c.JSON(http.StatusBadRequest, output)
+	}
+	output["addr"] = ethaddr
+	return c.JSON(http.StatusOK, output)
+}
