a092586b9c188b3c4faea588a5b32122c41cc749
zhangwm <442798+zhangwm404@users.noreply.github.com>
2023-01-16 11:02:45 +0800

    accept/return json object for trx.Data

--- pkg/chainapi/handlers/posttogroup.go
+++ pkg/chainapi/handlers/posttogroup.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 
@@ -20,7 +20,7 @@ type PostToGroupParam struct {
 		}
 	}
 	*/
-	Data string `json:"data" validate:"required"`
+	Data map[string]interface{} `json:"data" validate:"required"` // json object
 }
 
 type TrxResult struct {
@@ -28,15 +28,14 @@ type TrxResult struct {
 }
 
 func PostToGroup(payload *PostToGroupParam) (*TrxResult, error) {
-	data, err := base64.StdEncoding.DecodeString(payload.Data)
-	if err != nil {
-		return nil, err
-	}
-
 	groupmgr := chain.GetGroupMgr()
 	group, ok := groupmgr.Groups[payload.GroupId]
 	if !ok {
 		return nil, errors.New(fmt.Sprintf("Group %s not exist", payload.GroupId))
+	}
+	data, err := json.Marshal(payload.Data)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Invalid Data field, not json object, json.Marshal failed: %s", err))
 	}
 
 	trxId, err := group.PostToGroup(data)
