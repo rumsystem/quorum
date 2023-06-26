d7ebcc54e55f5fe41da370b9fa01dd3fe0502be5
zhangwm <442798+zhangwm404@users.noreply.github.com>
2023-06-26 21:15:04 +0800

    add memory stats to node rest api

--- pkg/chainapi/handlers/getnodeinfo.go
+++ pkg/chainapi/handlers/getnodeinfo.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"runtime"
 	"strings"
 
 	p2pcrypto "github.com/libp2p/go-libp2p/core/crypto"
@@ -17,6 +19,39 @@ type NodeInfo struct {
 	NodeType      string              `json:"node_type" validate:"required" example:"peer"`
 	NodeVersion   string              `json:"node_version" validate:"required" example:"1.0.0 - 99bbd8e65105c72b5ca57e94ae5be117eaf05f0d"`
 	Peers         map[string][]string `json:"peers" validate:"required"` // Example: {"/quorum/nevis/meshsub/1.1.0": ["16Uiu2HAmM4jFjs5EjakvGgJkHS6Lg9jS6miNYPgJ3pMUvXGWXeTc"]}
+	Mem           NodeInfoMem         `json:"mem"`
+}
+
+type ByteSize uint64
+type NodeInfoMem struct {
+	Sys        ByteSize `json:"sys"` // OS memory being used
+	HeapSys    ByteSize `json:"heap_sys"`
+	HeapAlloc  ByteSize `json:"heap_alloc"`
+	HeapInuse  ByteSize `json:"heap_inuse"`
+	StackSys   ByteSize `json:"stack_sys"`
+	StackInuse ByteSize `json:"stack_inuse"`
+	NumGC      uint32   `json:"num_gc"`
+}
+
+func (v ByteSize) MarshalJSON() ([]byte, error) {
+	i := int64(v)
+	s := b2m(i)
+	return json.Marshal(s)
+}
+
+func b2m(i int64) string {
+	const unit = 1024
+
+	if i < unit {
+		return fmt.Sprintf("%d B", i)
+	}
+
+	div, exp := int64(unit), 0
+	for n := i / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB", float64(i)/float64(div), "KMGTPE"[exp])
 }
 
 func updateNodeStatus(nodenetworkname string) {
@@ -58,6 +93,18 @@ func GetNodeInfo(networkName string) (*NodeInfo, error) {
 
 	peers := nodectx.GetNodeCtx().PeersProtocol()
 	info.Peers = *peers
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	info.Mem = NodeInfoMem{
+		Sys:        ByteSize(m.Sys),
+		HeapSys:    ByteSize(m.HeapSys),
+		HeapAlloc:  ByteSize(m.HeapAlloc),
+		HeapInuse:  ByteSize(m.HeapInuse),
+		StackSys:   ByteSize(m.StackSys),
+		StackInuse: ByteSize(m.StackInuse),
+		NumGC:      m.NumGC,
+	}
 
 	return &info, nil
 }
