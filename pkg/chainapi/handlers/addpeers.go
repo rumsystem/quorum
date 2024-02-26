d6cbeef2ee5d4ab1b2d0c51fadc9dd445aee8396
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2024-02-26 14:42:49 +0800

    update readme

--- pkg/chainapi/handlers/addpeers.go
+++ pkg/chainapi/handlers/addpeers.go
@@ -7,7 +7,7 @@ import (
 )
 
 // AddPeerParam list of multiaddr
-type AddPeerParam []string // Example: ["/ip4/94.23.17.189/tcp/10666/p2p/16Uiu2HAmGTcDnhj3KVQUwVx8SGLyKBXQwfAxNayJdEwfsnUYKK4u", "/ip4/132.145.109.63/tcp/10666/p2p/16Uiu2HAmTovb8kAJiYK8saskzz7cRQhb45NRK5AsbtdmYsLfD3RM"]
+type AddPeerParam []string // Example: ["/ip4/94.23.17.189/tcp/62777/p2p/16Uiu2HAm5waftP3s4oE1EzGF2SyWeK726P5B8BSgFJqSiz6xScGz", "/ip4/132.145.109.63/tcp/10666/p2p/16Uiu2HAmTovb8kAJiYK8saskzz7cRQhb45NRK5AsbtdmYsLfD3RM"]
 
 type AddPeerResult struct {
 	SuccCount int               `json:"succ_count" example:"100"`
